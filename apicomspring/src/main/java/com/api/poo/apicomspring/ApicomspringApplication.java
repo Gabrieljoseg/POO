d8041d99ac7795e2e66a62b5d001aa613413a3af
Gabriel José Gomes <gabrieljgomes9@gmail.com>
2023-06-16 22:47:47 +0000

    first-commit

---
+++ apicomspring/src/main/java/com/api/poo/apicomspring/ApicomspringApplication.java
@@ -0,0 +1,13 @@
+package com.api.poo.apicomspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicomspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicomspringApplication.class, args);
+	}
+
+}
---
+++ apicomspring/src/test/java/com/api/poo/apicomspring/ApicomspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.poo.apicomspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicomspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
