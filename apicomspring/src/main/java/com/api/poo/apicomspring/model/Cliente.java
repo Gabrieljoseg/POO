8112e6e6d203a3de3840eeb4f3e211087a682799
Gabriel José Gomes <gabrieljgomes9@gmail.com>
2023-06-16 23:20:46 +0000

    ok

--- apicomspring/src/main/java/com/api/poo/apicomspring/model/Cliente.java
+++ apicomspring/src/main/java/com/api/poo/apicomspring/model/Cliente.java
@@ -27,22 +27,21 @@ public class Cliente {
     private String password;
 
     public Cliente (Long id, String name, String email, String password) {
-        this.id =id;
-        this.name =name;
-        this.email =email;
-        this.password =password;
+        this.id=id;
+        this.name=name;
+        this.email=email;
+        this.password=password;
     }
-
-@Override
-public int hashCode() {
+   
+    public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + ((id == null) ? 0 : id.hashCode());
         return result;
     }
 
-@Override
-public boolean equals(Objetobj) {
+  
+    public boolean equals(Objetobj) {
         if (this ==obj)
             return true;
         else if (obj== null)
@@ -55,11 +54,13 @@ public boolean equals(Objetobj) {
         else return id.equals(other.id);
     }
 
+
+
     public Long getId() {
         return id;
     }
 
-    public void setId(Longid) {
+    public void setId(Long id) {
         this.id =id;
     }
 
@@ -67,7 +68,7 @@ public boolean equals(Objetobj) {
         return name;
     }
 
-    public void setName(Stringname) {
+    public void setName(String name) {
         this.name =name;
     }
 
@@ -75,7 +76,7 @@ public boolean equals(Objetobj) {
         return email;
     }
 
-    public void setEmail(Stringemail) {
+    public void setEmail(String email) {
         this.email =email;
     }
 
@@ -83,7 +84,7 @@ public boolean equals(Objetobj) {
         return password;
     }
 
-    public void setPassword(Stringpassword) {
+    public void setPassword(String password) {
         this.password =password;
     }
     
